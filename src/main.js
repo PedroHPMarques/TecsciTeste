6b927eec3e83b05b1837083856167c095641069d
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2024-08-12 17:32:53 -0300

    adiciona dependencias vue date-picker

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import { createPinia } from 'pinia'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-
+app.component('VueDatePicker', VueDatePicker)
 
 app.mount('#app')
