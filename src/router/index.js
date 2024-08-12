22a110aa53f8034753cdc606b3e9d99f55fea310
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2024-08-12 17:26:03 -0300

    adiciona rota para Nomes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
+import Nomes from '@/views/Nomes.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: '/',
       component: Home
+    },
+    {
+      path: '/nomes',
+      name: '/nomes',
+      component: Nomes
     },
   ]
 })
